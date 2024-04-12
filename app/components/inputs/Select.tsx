066adf5c0d1a6ff45381ebc42cd63d8798a8bf1c
Romit <romit2884@gmail.com>
2024-04-12 22:05:45 +0530

    update select component

--- app/components/inputs/Select.tsx
+++ app/components/inputs/Select.tsx
@@ -1,6 +1,7 @@
-'use client';
+"use client";
 
-import ReactSelect from 'react-select'
+import clsx from "clsx";
+import ReactSelect from "react-select";
 
 interface SelectProps {
   label: string;
@@ -10,14 +11,8 @@ interface SelectProps {
   disabled?: boolean;
 }
 
-const Select: React.FC<SelectProps> = ({
-  label,
-  value,
-  onChange,
-  options,
-  disabled,
-}) => {
-  return ( 
+const Select: React.FC<SelectProps> = ({ label, value, onChange, options, disabled }) => {
+  return (
     <div className="z-[100]">
       <label
         className="
@@ -26,28 +21,45 @@ const Select: React.FC<SelectProps> = ({
           font-medium 
           leading-6 
           text-gray-900
+          dark:text-gray-200
         "
       >
         {label}
       </label>
       <div className="mt-2">
-      <ReactSelect
-        isDisabled={disabled}
-        value={value}
-        onChange={onChange}
-        isMulti
-        options={options}
-        menuPortalTarget={document.body}
-        styles={{
-          menuPortal: (base) => ({ ...base, zIndex: 9999 })
-        }}
-        classNames={{
-          control: () => 'text-sm',
-        }}
-      />
+        <ReactSelect
+          isDisabled={disabled}
+          value={value}
+          onChange={onChange}
+          isMulti
+          options={options}
+          menuPortalTarget={document.body}
+          styles={{
+            menuPortal: (base) => ({ ...base, zIndex: 9999 }),
+            option: (base, { isFocused, isSelected }) => ({
+              ...base,
+              backgroundColor: isFocused
+                ? "rgb(107 114 128)"
+                : isSelected
+                ? "rgb(156 163 175)"
+                : "#3a3b3c",
+            }),
+          }}
+          classNames={{
+            control: (state) =>
+              clsx(
+                `
+                text-sm 
+                dark:bg-lightgray 
+                dark:border-gray-500`,
+                state.isFocused && "border-gray-400"
+              ),
+            menu: () => "dark:bg-lightgray",
+          }}
+        />
       </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Select;

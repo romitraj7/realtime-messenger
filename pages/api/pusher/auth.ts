ced97ad05cab9e5de3e504e5a36bb4689b136a69
Romit <romit2884@gmail.com>
2024-04-13 00:51:03 +0530

    update pusher auth

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -1,8 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 
-import { authOptions } from "../../../app/api/auth/[...nextauth]/route";
 import { pusherServer } from "../../../app/libs/pusher";
+import authOptions from "@/app/libs/auth";
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
   const session = await getServerSession(request, response, authOptions);

6e6407525f301a0ed29e058ef0311ad6230ca471
Romit <romit2884@gmail.com>
2024-04-12 22:05:19 +0530

    update Avatar Component

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -3,18 +3,58 @@
 import { User } from "@prisma/client";
 import Image from "next/image";
 import placeholder from '@/public/images/placeholder.jpg'
-interface AvatarProps{
-    user?: User
+import useActiveList from "../hooks/useActiveList";
+
+interface AvatarProps {
+  user?: User;
 }
-const Avatar: React.FC<AvatarProps> = ({user}) => {
-    return ( 
-        <div className="relative">
-            <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11 ">
-                <Image alt="Avatar" src={user?.image || placeholder} fill/>
-            </div>
-            <span className="absolute block rounded-full bg-green-500 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3"/>
-        </div>
-     );
-}
- 
+
+const Avatar: React.FC<AvatarProps> = ({ user }) => {
+  const { members } = useActiveList();
+  const isActive = members.indexOf(user?.email!) !== -1;
+
+  return (
+    <div className="relative h-9 md:h-11">
+      <div
+        className="
+        relative 
+        inline-block 
+        rounded-full 
+        overflow-hidden
+        h-9 
+        w-9 
+        md:h-11 
+        md:w-11
+      "
+      >
+        <Image
+          className="object-cover"
+          fill
+          src={user?.image || placeholder}
+          alt="Avatar"
+        />
+      </div>
+      {isActive && (
+        <span
+          className="
+            absolute 
+            block 
+            rounded-full 
+            bg-green-500 
+            ring-2 
+            ring-white 
+            top-0 
+            right-0
+            h-2 
+            w-2 
+            md:h-3 
+            md:w-3
+            dark:ring-lightgray
+          "
+        />
+      )}
+    </div>
+  );
+};
+
 export default Avatar;
